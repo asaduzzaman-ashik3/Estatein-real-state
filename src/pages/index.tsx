b78f6eacc2b2603cf3bfbb7a147ec6201bd98ea2
Assaduzzaman Ashik <ashikhossain372@gmail.com>
2026-01-03 02:13:36 +0600

    add hero cards

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -11,7 +11,20 @@ export default function Home() {
             <button className="text-white border-2 border-[#262626] px-6 py-4 rounded-lg">Learn More</button>
             <button className="text-white bg-purple-600 px-6 py-4 rounded-lg">Browse Properties</button>
           </div>
-
+          <div className="flex items-center gap-4">
+            <div className="flex-1 bg-[#1A1A1A] border-2 border-[#262626] rounded-lg  px-6 py-4">
+              <h1 className="text-[40px]">200+</h1>
+              <p className="text-gray-600 text-md">Happy Customer</p>
+            </div>
+            <div className="flex-1 bg-[#1A1A1A] border-2 border-[#262626] rounded-lg  px-6 py-4">
+              <h1 className="text-[40px]">150+</h1>
+              <p className="text-gray-600 text-md">Projects Completed</p>
+            </div>
+            <div className="flex-1 bg-[#1A1A1A] border-2 border-[#262626] rounded-lg px-6 py-4">
+              <h1 className="text-[40px]">300+</h1>
+              <p className="text-gray-600 text-md">Clients Served</p>
+            </div>
+          </div>
         </div>
         <div>
           <Image
