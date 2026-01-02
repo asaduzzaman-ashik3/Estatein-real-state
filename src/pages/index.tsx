905f46bfa7803937f7d9d52da1c99cf131802e26
Assaduzzaman Ashik <ashikhossain372@gmail.com>
2026-01-03 02:36:10 +0600

    circle image add

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,10 +3,12 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div className="bg-[#1E1E1E] text-white">
-      <div className="w-10/12 h-dvh mx-auto flex justify-between items-center gap-12">
-        <div className="flex-col space-y-4">
-          <h1 className="text-[60px]"  >Discover Your Dream<br />Property with Estatein</h1>
+      <div className="flex justify-between items-center gap-12 ps-36 relative">
+        <div className="flex-col space-y-10 w-1/2">
+          <div>
+            <h1 className="text-[60px]"  >Discover Your Dream<br />Property with Estatein</h1>
           <p className="text-lg text-gray-400 font-extralight">Your journey to finding the perfect property begins here. Explore our listings to find the home that matches your dreams.</p>
+          </div>
           <div className="space-x-5">
             <button className="text-white border-2 border-[#262626] px-6 py-4 rounded-lg">Learn More</button>
             <button className="text-white bg-purple-600 px-6 py-4 rounded-lg">Browse Properties</button>
@@ -26,12 +28,20 @@ export default function Home() {
             </div>
           </div>
         </div>
-        <div>
+        <div className="w-1/2"> 
           <Image
             src="/images/hero-img.png"
             alt="Hero Image"
-            width={814}
-            height={920}
+            width={1114}
+            height={1220}
+          />
+        </div>
+        <div className="absolute top-2/7 left-2/4 ms-24 transform -translate-x-1/2 -translate-y-1/2 z-20 w-34 h-34">
+          <Image
+            src="/images/hero-circle-img.png"
+            alt="Hero Circle"
+            fill
+            className="object-cover rounded-full"
           />
         </div>
       </div>
